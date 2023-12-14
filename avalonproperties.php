e77c00a4a748e4ae2c87a4d626aab6ce7b9e9389
Priyankakarthi <153817210+Priyankakarthi@users.noreply.github.com>
2023-12-14 03:11:44 -0800

    first file

---
+++ avalonproperties.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="robots" content="index, follow" />
+<meta name="viewport" content="width=1480">
+<link rel="alternate" href="https://kwjerseyshorerentals.com/mobile/avalonproperties.php" />
+<meta name="keywords" content="Keller Williams Realty, Jersey Shore, KW, Vacation Rentals, Rental Properties, Property Management, Wildwoods, Ocean City, Sea Isle City, Cape May, Avalon, Stone Harbor, Jersey Shore Properties, Avalon New Jersey, Properties For Sale, " />
+<meta name="description" content="Avalon properties for sale with information provided by Keller Williams Jersey Shore." />
+<title>Avalon New Jersey | Real Estate Properties For Sale | Keller Williams Jersey Shore</title>
+<link href="styles.css" rel="stylesheet" type="text/css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+<link rel="SHORTCUT ICON" href="images/kw.ico">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TGSXGSH8P8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TGSXGSH8P8');
+</script>
+</head>
+
+<body>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td align="center" bgcolor="#333333"><table width="90%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td colspan="2" align="left" valign="middle"><img src="images/t.gif" width="25" height="24" alt="" border="0" /></td>
+            </tr>
+          <tr>
+            <td align="left" valign="middle"><a href="index.php"><img src="images/kellerwilliamsjerseyshore.png" alt="KW Jersey Shore - Keller Williams Realty" width="307" height="72" border="0" /></a></td>
+            <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td align="right" valign="top"><a href="tel:18445974673"><img src="images/phone.png" width="170" height="34" alt="Call Today" border="0" /></a></td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td align="right" valign="bottom"><a href="#" class="whitelink">SALES<img src="images/navigationarrow.png" width="14" height="15" align="absmiddle" alt="Navigation Arrow" border="0" /></a> &nbsp; &nbsp; &nbsp; <a href="#" class="whitelink">VACATION RENTALS<img src="images/navigationarrow.png" width="14" height="15" align="absmiddle" alt="Navigation Arrow" border="0" /></a> &nbsp; &nbsp; &nbsp; <a href="#" class="whitelink">ABOUT<img src="images/navigationarrow.png" width="14" height="15" align="absmiddle" alt="Navigation Arrow" border="0" /></a> &nbsp; &nbsp; &nbsp; <a href="#" class="whitelink">AREA GUIDE<img src="images/navigationarrow.png" width="14" height="15" align="absmiddle" alt="Navigation Arrow" border="0" /></a> &nbsp; &nbsp; &nbsp; <a href="#" class="whitelink">CONTACT</a></td>
+              </tr>
+            </table></td>
+          </tr>
+          <tr>
+            <td colspan="2" align="left" valign="middle"><img src="images/t.gif" width="25" height="24" alt="" border="0" /></td>
+            </tr>
+        </table></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center" valign="middle"><table width="90%" border="0" cellpadding="0" cellspacing="0" class="medspacing">
+      <tr>
+        <td><img src="images/t.gif" width="30" height="40" alt="" border="0" /></td>
+      </tr>
+      <tr>
+        <td align="center" valign="top"><h1>Avalon New Jersey Properties For Sale
+          </h1>
+          <p><img src="images/pagebreak.png" width="387" height="40" alt="Line Break" border="0" /></p>
+          <p class="size20"><a href="jerseyshoreproperties.php">All Jersey Shore Properties</a> | <a href="oceancitynjproperties.php">Ocean City</a> | <a href="wildwoodproperties.php">Wildwood</a> | <a href="seaislecityproperties.php">Sea Isle City</a> | <a href="avalonproperties.php">Avalon</a> | <a href="stoneharborproperties.php">Stone Harbor</a> | <a href="brigantineproperties.php">Brigantine</a> | <a href="margateproperties.php">Margate</a> | <a href="capemayproperties.php">Cape May</a></p>
+          <p><img src="images/pagebreak.png" width="387" height="40" alt="Line Break" border="0" /></p>
+          <p>**Note: The properties will display in the same format as shown on <a href="mls.php">mls.php</a>**</p></td>
+      </tr>
+      <tr>
+        <td><img src="images/t.gif" width="30" height="40" alt="" border="0" /></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center" valign="middle" bgcolor="#333333"><table width="468" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><img src="images/t.gif" width="30" height="30" alt="" border="0" /></td>
+      </tr>
+      <tr>
+        <td align="center" valign="middle"><a href="index.php"><img src="images/kellerwilliamsrealty.png" alt="KW Jersey Shore - Keller Williams Realty" width="468" height="108" border="0" /></a></td>
+      </tr>
+      <tr>
+        <td><img src="images/t.gif" width="30" height="30" alt="" border="0" /></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center" valign="middle" bgcolor="#232323"><table width="90%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><img src="images/t.gif" width="30" height="20" alt="" border="0" /></td>
+      </tr>
+      <tr>
+        <td align="center" valign="middle"><a href="locations.php" class="whitelink">903 Bay Avenue, Ocean City, NJ</a><img src="images/navdot.png" alt="Dot Spacer" width="53" height="28" border="0" align="absmiddle" /><a href="tel:8445974673" class="whitelink">Local: 844-597-4673</a><img src="images/navdot.png" alt="Dot Spacer" width="53" height="28" border="0" align="absmiddle" /><a href="contact.php" class="whitelink">reservations@kwjerseyshore.com</a><img src="images/navdot.png" alt="Dot Spacer" width="53" height="28" border="0" align="absmiddle" /><a href="https://www.facebook.com/KWJerseyShoreRentals/" target="_blank"><img src="images/facebook.png" alt="Like Us On Facebook" width="30" height="28" align="absmiddle" border="0" /></a><img src="images/t.gif" alt="" width="10" height="28" border="0" align="absmiddle" /><a href="https://twitter.com/kwjsrentals" target="_blank"><img src="images/twitter.png" alt="Follow Us On Twitter" width="30" height="28" align="absmiddle" border="0" /></a><img src="images/t.gif" alt="" width="10" height="28" border="0" align="absmiddle" /><a href="https://www.instagram.com/kwjsrentals/" target="_blank"><img src="images/instagram.png" alt="Follow Us On Instagram" width="30" height="28" align="absmiddle" border="0" /></a></td>
+      </tr>
+      <tr>
+        <td><img src="images/t.gif" width="30" height="20" alt="" border="0" /></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center" valign="middle" bgcolor="#333333"><table width="1000" border="0" cellpadding="0" cellspacing="0" class="smallspacing white size16">
+      <tr>
+        <td colspan="10"><img src="images/t.gif" width="30" height="30" alt="" border="0" /></td>
+      </tr>
+      <tr>
+        <td align="right" valign="top" class="size17"><strong><a href="index.php" class="whitelink">HOME</a></strong><br />
+          <strong><a href="contact.php" class="whitelink">CONTACT</a></strong><a href="contact.php"></a><br />
+          <strong><a href="sitemap.php" class="whitelink">SITE MAP</a></strong><br />
+          <strong><a href="terms.php" class="whitelink">TERMS</a></strong><a href="terms.php"></a><br />
+          <strong><a href="privacy.php" class="whitelink">PRIVACY</a></strong><a href="privacy.php"></a><br />
+          <strong><a href="login.php" class="whitelink">ADMIN</a></strong><a href="login.php"></a></td>
+        <td width="30" align="left" valign="top"><img src="images/t.gif" width="30" height="30" alt="" border="0" /></td>
+        <td width="30" align="left" valign="top" background="images/footerline.png"><img src="images/t.gif" width="30" height="30" alt="" border="0" /></td>
+        <td align="left" valign="top" class="white"><span class="size17"><strong>SALES</strong></span><br />
+          - <a href="featured.php" class="whitelink size16">Featured Listings</a><br />
+          - <a href="jerseyshoreproperties.php" class="whitelink size16">Jersey Shore Properties</a><br />
+          - <a href="mls.php" class="whitelink size16">Search MLS</a><br />
+          - <a href="sold.php" class="whitelink size16">Recently Sold</a><br />
+          - <a href="list.php" class="whitelink size16">List Your Property</a></td>
+        <td width="30" align="left" valign="top" background="images/footerline.png"><img src="images/t.gif" width="30" height="30" alt="" border="0" /></td>
+        <td align="left" valign="top" class="white"><span class="size17"><strong>VACATION RENTALS</strong></span><br />
+          - <a href="vacationrentals.php" class="whitelink size16">Rental Search</a><br />
+          - <a href="rentals.php" class="whitelink size16">All Rental Properties</a><br />
+          - <a href="help.php" class="whitelink size16">Let Us Help</a><br />
+          - <a href="listrental.php" class="whitelink size16">List Your Rental</a><br />
+          - <a href="propertymanagement.php" class="whitelink size16">Property Management</a><br />
+          - <a href="owners-tenants.php" class="whitelink size16">Owners / Tenants</a></td>
+        <td width="30" align="left" valign="top" background="images/footerline.png"><img src="images/t.gif" width="30" height="30" alt="" border="0" /></td>
+        <td align="left" valign="top" class="white"><span class="size17"><strong>ABOUT</strong></span><br />
+          - <a href="team.php" class="whitelink size16">Meet The Team</a><br />
+          - <a href="locations.php" class="whitelink size16">Office Locations</a><br />
+          - <a href="kw.php" class="whitelink size16">Why KW Jersey Shore</a><br />
+          - <a href="testimonials.php" class="whitelink size16">Testimonials</a><br />
+          - <a href="faq.php" class="whitelink size16">FAQ</a><br />
+          - <a href="policies.php" class="whitelink size16">Policies</a></td>
+        <td width="30" align="left" valign="top" background="images/footerline.png"><img src="images/t.gif" width="30" height="30" alt="" border="0" /></td>
+        <td align="left" valign="top" class="white"><span class="size17"><strong>AREA GUIDE</strong></span><br />
+          - <a href="jerseyshore.php" class="whitelink size16">The Jersey Shore</a><br />
+          - <a href="wildwood.php" class="whitelink size16">The Wildwoods</a><br />
+          - <a href="oceancitynj.php" class="whitelink size16">Ocean City</a><br />
+          - <a href="seaislecity.php" class="whitelink size16">Sea Isle City</a><br />
+          - <a href="capemay.php" class="whitelink size16">Cape May</a><br />
+          - <a href="stoneharboravalon.php" class="whitelink size16">Stone Harbor &amp; Avalon</a></td>
+      </tr>
+      <tr>
+        <td colspan="10"><img src="images/t.gif" width="30" height="30" alt="" border="0" /></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center" valign="middle"><table width="80%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td colspan="2"><img src="images/t.gif" width="30" height="30" alt="" border="0" /></td>
+        </tr>
+      <tr>
+        <td align="left" valign="middle" class="copyright">© 2022 | Keller Williams Realty Jersey Shore - All Rights Reserved.</td>
+        <td align="right" valign="middle" class="copyright">Web Site Designed By: <a href="https://designsquare1.com" target="_blank" class="copyrightlink">Square 1 Design</a></td>
+      </tr>
+      <tr>
+        <td colspan="2"><img src="images/t.gif" width="30" height="30" alt="" border="0" /></td>
+        </tr>
+    </table></td>
+  </tr>
+</table>
+</body>
+</html>
